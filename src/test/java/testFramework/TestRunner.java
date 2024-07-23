94a1f6789faf8e58311b66f630ef570e96d24e8e
christian harborow <christianharborow@hotmail.co.uk>
2024-07-23 13:58:32 +0100

    adds tag filter to testRunner

--- src/test/java/testFramework/TestRunner.java
+++ src/test/java/testFramework/TestRunner.java
@@ -1,15 +1,20 @@
 package testFramework;
 
 import io.cucumber.junit.platform.engine.Constants;
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("testFramework/stepDefs")
 @ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/features")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "testFramework/stepDefs")
+@IncludeTags({
+        "Course",
+        "Login",
+        "Spartan",
+//        "Get",
+//        "Post",
+//        "Put"
+})
 public class TestRunner {
 }
